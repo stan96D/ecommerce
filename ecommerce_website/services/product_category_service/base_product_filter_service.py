d27669ea4df07d83d7fa720727fa12dae4113df3
stan <stan-deckers@live.nl>
2024-03-22 13:47:36 +0100

    Added new ProductFilterService and Interface

---
+++ ecommerce_website/services/product_category_service/base_product_filter_service.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class ProductFilterServiceInterface():
+    @abstractmethod
+    def get_product_filter_by_id(product_filter_name):
+        pass
+
+    @abstractmethod
+    def get_product_filters_by_category_id(product_category_id):
+        pass
+
+
+    @abstractmethod
+    def get_product_filter_by_name(product_filter_name):
+        pass
+
+    @abstractmethod
+    def get_all_product_filters():
+        pass
---
+++ ecommerce_website/services/product_category_service/product_filter_service.py
@@ -0,0 +1,36 @@
+from ecommerce_website.models import ProductFilter
+from ecommerce_website.services.product_category_service.base_product_filter_service import ProductFilterServiceInterface
+
+
+class ProductFilterService(ProductFilterServiceInterface):
+    @staticmethod
+    def get_product_filter_by_id(product_filter_name):
+        try:
+            return ProductFilter.objects.get(id=product_filter_name)
+        except ProductFilter.DoesNotExist:
+            return None
+
+    @staticmethod
+    def get_product_filters_by_category_id(product_category_id):
+        try:
+            return ProductFilter.objects.filter(parent_category_id=product_category_id)
+        except ProductFilter.DoesNotExist:
+            return None
+
+    @staticmethod
+    def get_product_filter_by_name(product_filter_name):
+        try:
+            return ProductFilter.objects.get(name=product_filter_name)
+        except ProductFilter.DoesNotExist:
+            return None
+
+    @staticmethod
+    def get_all_product_filters():
+        try:
+            return ProductFilter.objects.all()
+        except ProductFilter.DoesNotExist:
+            return None
+
+
+
+
