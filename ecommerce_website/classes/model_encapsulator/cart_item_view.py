9fce2fd8f2844c7668c063066a82e26f35629ec6
stan <stan-deckers@live.nl>
2024-05-28 21:20:36 +0200

    Changing and visualizing CartItemView and ProductView/ProductDetailView

--- ecommerce_website/classes/model_encapsulator/cart_item_view.py
+++ ecommerce_website/classes/model_encapsulator/cart_item_view.py
@@ -1,3 +1,5 @@
+from ecommerce_website.classes.helpers.surface_area_calculator import SurfaceAreaCalculator
+
 class CartItemView:
     def __init__(self, product, quantity):
         attributes_dict = {}
@@ -7,13 +9,19 @@ class CartItemView:
         self.id = product.id
         self.name = product.name
         self.price = product.selling_price
+        self.unit_price = product.unit_selling_price
         self.attributes = attributes_dict
         self.thumbnail = product.thumbnail
         self.quantity = quantity
         self.stock = product.stock.quantity
-        self.totalPrice = quantity * product.selling_price
+        self.totalPrice = quantity * product.unit_selling_price
 
+        surface = attributes_dict['Oppervlakte']
+        self.surface = SurfaceAreaCalculator.parse_surface_area(surface)
 
+        brand = attributes_dict['Merk']
+        self.brand = brand
+        
 class CartView:
     def __init__(self, total_price, sub_price, tax_price_high, tax_price_low, shipping_price):
 
--- ecommerce_website/classes/model_encapsulator/product_view.py
+++ ecommerce_website/classes/model_encapsulator/product_view.py
@@ -1,3 +1,5 @@
+from ecommerce_website.classes.helpers.surface_area_calculator import SurfaceAreaCalculator
+import json 
 
 class ProductView:
     def __init__(self, product):
@@ -5,14 +7,20 @@ class ProductView:
         self.id = product.id
         self.name = product.name
         self.price = product.selling_price
+        self.price_per_pack = product.unit_selling_price
+
         self.thumbnail = product.thumbnail
         self.images = product.images
         self.quantity = product.stock.quantity
         self.is_runner = product.runner
 
+        brand = product.attributes.filter(
+            attribute_type__name='Merk').first().value
+
+        self.brand = brand
+
         if product.has_product_sale:
             self.sale_price = product.sale_price
-
 
 
 class ProductDetailView:
@@ -20,12 +28,64 @@ class ProductDetailView:
         attributes_dict = {}
         for attribute in product.attributes.all():
             attributes_dict[attribute.attribute_type.name] = attribute.value
-        print("TEXTEXTEXTETX", product.search_string)
 
         self.id = product.id
         self.name = product.name
+        self.price_per_pack = product.unit_selling_price
         self.price = product.selling_price
-        self.attributes = attributes_dict
         self.thumbnail = product.thumbnail
         self.images = product.images
         self.quantity = product.stock.quantity
+        self.description = attributes_dict.get('Omschrijving', '')
+        self.surface = SurfaceAreaCalculator.parse_surface_area(
+            attributes_dict.get('Oppervlakte', ''))
+        self.brand = attributes_dict.get('Merk', '')
+
+        # Initialize product specifications
+        product_specifications = {
+            "Algemene Specificaties": {},
+            "Overige Specificaties": {},
+            "Aanvullende Informatie": {},
+            "Omschrijving": {}
+        }
+
+        # Extract attributes and organize them into sections
+        for attribute in product.attributes.all():
+            attribute_type = attribute.attribute_type.name
+            attribute_value = attribute.value
+
+            if attribute_type in ["Afmeting", "Totale Dikte", "Pakinhoud", "Oppervlakte", "Naam", "Type", "Collectie", "Kleur", "Merk", "Vloertype"]:
+                product_specifications["Algemene Specificaties"][attribute_type] = attribute_value
+            elif attribute_type in ["Omschrijving"]:
+                product_specifications["Omschrijving"][attribute_type] = attribute_value
+
+            elif attribute_type in ["Links"]:
+                try:
+                    links_dict = json.loads(attribute_value)
+                except json.JSONDecodeError:
+                    attribute_value = attribute_value.replace("'", '"')
+                    links_dict = json.loads(attribute_value)
+
+                product_specifications["Aanvullende Informatie"] = links_dict
+            else:
+                product_specifications["Overige Specificaties"][attribute_type] = attribute_value
+
+
+
+            # Combine all sections into a list
+            self.sections = [
+                {"Omschrijving": product_specifications["Omschrijving"]},
+
+                {"Algemene Specificaties":
+                    product_specifications["Algemene Specificaties"]},
+
+                {"Overige Specificaties":
+                    product_specifications["Overige Specificaties"]},
+                {"Aanvullende Informatie":
+                    product_specifications["Aanvullende Informatie"]},
+
+            ]
+
+
+
+
