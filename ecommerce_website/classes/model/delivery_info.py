0a8613296c46b1144cafbe4b5e77905d5b329b28
stan <stan-deckers@live.nl>
2024-05-28 21:12:24 +0200

    Added date formatting

--- ecommerce_website/classes/model/delivery_info.py
+++ ecommerce_website/classes/model/delivery_info.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
+
 class DeliveryInfo:
     def __init__(self, delivery_method, delivery_date):
         self.delivery_method = delivery_method
-        self.delivery_date = delivery_date
-        
+        self.delivery_date = datetime.strptime(
+            delivery_date, '%d-%m-%Y').strftime('%Y-%m-%d')
+
     def serialize(self):
         return {
             'delivery_method': self.delivery_method,
