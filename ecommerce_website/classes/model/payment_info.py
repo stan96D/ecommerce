15010db027f6695640a96b871d0d32a463fec011
stan <stan-deckers@live.nl>
2024-06-21 14:11:33 +0200

    Changed PaymentInfo

--- ecommerce_website/classes/model/payment_info.py
+++ ecommerce_website/classes/model/payment_info.py
@@ -1,17 +1,21 @@
 class PaymentInfo:
-    def __init__(self, payment_method, bank):
+    def __init__(self, payment_method, bank, bank_id, payment_method_id):
         self.payment_method = payment_method
+        self.payment_method_id = payment_method_id
         self.bank = bank
+        self.bank_id = bank_id
 
     def serialize(self):
         return {
-            'payment_method': self.payment_method,
+            'payment_method': self.payment_method,    
+            'payment_method_id': self.payment_method_id,
             'bank': self.bank,
+            'bank_id': self.bank_id
         }
 
     @property
     def payment_information(self):
         if self.bank:
-            return "{}\n{}".format(self.payment_method, self.bank)
+            return "{}\n{}".format(self.payment_method, self.payment_method_id, self.bank)
         else:
-            return "{}".format(self.payment_method)
+            return "{}".format(self.payment_method, self.payment_method_id)
