11da48a50f82bbc68e3dd6d0665711d34725ee9b
stan <stan-deckers@live.nl>
2024-09-21 18:03:40 +0200

    updated wsgi.py

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -1,15 +1,9 @@
-"""
-WSGI config for ecommerce project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+import sys
+
+# Add the project directory to the sys.path
+sys.path.append('/var/www/ecommerce_test')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
