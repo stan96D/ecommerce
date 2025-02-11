25e694480797550d6253d5fbf7691fa00d5af6fc
stan <stan-deckers@live.nl>
2025-02-11 20:37:30 +0100

    fix: add env var loading for mod_wsgi

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+import sys
+from dotenv import load_dotenv
 
+# Define the base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Load environment variables from .env
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
+
+# Ensure the project directory is in the system path
+sys.path.append(BASE_DIR)
+
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
+# Get WSGI application
 application = get_wsgi_application()
