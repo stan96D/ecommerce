30a380803f70cf3c6e6d30a0a7bc9f31915ec4a5
stan <stan-deckers@live.nl>
2025-03-02 19:27:41 +0100

    test: test for sentry

--- ecommerce_website/urls.py
+++ ecommerce_website/urls.py
@@ -12,7 +12,12 @@ sitemaps = {
 handler404 = 'ecommerce_website.views.custom_404_view'
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
 
     # API
     path('get-address/',
