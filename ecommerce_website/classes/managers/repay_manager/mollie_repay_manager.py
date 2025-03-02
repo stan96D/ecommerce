d25abc21192f35fe8f6e4fa8bf43017bbd94987a
stan <stan-deckers@live.nl>
2025-03-02 19:03:57 +0100

    fix: url for repaying

--- ecommerce_website/classes/managers/repay_manager/mollie_repay_manager.py
+++ ecommerce_website/classes/managers/repay_manager/mollie_repay_manager.py
@@ -1,20 +1,25 @@
+from ecommerce_website.classes.helpers.env_loader import EnvLoader
 from ecommerce_website.classes.managers.payment_manager.mollie_client import MollieClient
+from ecommerce_website.classes.managers.url_manager.url_manager import EncapsulatedURLManager
 from ecommerce_website.services.order_service.base_order_service import OrderServiceInterface
 from ecommerce_website.classes.managers.repay_manager.base_repay_manager import RepayManagerInterface
 from django.conf import settings
 
+
 class MollieRepayManager(RepayManagerInterface):
 
     def repay(self, order_id, order_service: OrderServiceInterface):
+
+        environment = EnvLoader.get_env()
+        url_manager = EncapsulatedURLManager.get_url_manager(environment)
 
         order = order_service.get_order_by_id(order_id)
 
         payment_method = order.payment_information_id
         issuer_id = order.payment_issuer
 
-        # For Mollie testing purposes
-        redirect_url = f"https://{settings.NGROK_URL}/order_detail?order_id={order.token}"
-        webhook_url = f"https://{settings.NGROK_URL}/mollie_webhook/"
+        webhook_url = url_manager.create_webhook()
+        redirect_url = url_manager.create_redirect(order.token)
 
         payment = MollieClient().create_payment('EUR', str(
             order.total_price), order.order_number, redirect_url, webhook_url, payment_method, issuer_id)
