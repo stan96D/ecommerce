ad7dde7da1787ed905738826c03f6950993ad1c4
stan <stan-deckers@live.nl>
2025-03-14 19:11:58 +0100

    feat: update product/category data

--- ecommerce_website/seeders/initial_seeder_data/category_data.py
+++ ecommerce_website/seeders/initial_seeder_data/category_data.py
@@ -4,7 +4,7 @@ category_data = [
         "active": True,
         "for_homepage": True,
         "description": "Duurzaam, veelzijdig en gemakkelijk te onderhouden. Onze PVC vloeren bieden een uitstekende oplossing voor elk interieur, van woningen tot commerciële ruimtes. Ze zijn waterbestendig, slijtvast, en bieden een luxe uitstraling die perfect past bij moderne interieurs. Kies voor PVC vloeren voor een combinatie van stijl, comfort en praktische voordelen.",
-        "thumbnail": "category_thumbnails/pvc_category.webp",
+        "thumbnail": "category_thumbnails/pvc-category.jpeg",
         "subcategories": []
     },
     {
@@ -12,7 +12,7 @@ category_data = [
         "active": True,
         "for_homepage": True,
         "description": "Laminaatvloeren zijn een populaire keuze vanwege hun betaalbaarheid en veelzijdigheid. Met een breed scala aan stijlen, van houtlook tot tegellook, kunnen ze naadloos passen in elk type interieur. Onze laminaatvloeren zijn niet alleen makkelijk te onderhouden, maar ook extreem kras- en slijtvast, waardoor ze perfect zijn voor drukbezochte gebieden in je huis.",
-        "thumbnail": "category_thumbnails/laminaat_category.jpg",
+        "thumbnail": "category_thumbnails/laminaat-category.jpeg",
         "subcategories": []
     },
     {
@@ -20,7 +20,7 @@ category_data = [
         "active": True,
         "for_homepage": True,
         "description": "Voor wie de warme uitstraling van echt hout wil, maar met de voordelen van een duurzamer product, is lamelparket de ideale keuze. Het bestaat uit meerdere lagen, met een echte houten toplaag, die zowel de charme van massief hout biedt als extra stabiliteit. Perfect voor zowel moderne als klassieke interieurs, en gemakkelijk te onderhouden voor een langdurige schoonheid.",
-        "thumbnail": "category_thumbnails/lamelparket-category.jpg",
+        "thumbnail": "category_thumbnails/lamelparket-category.jpeg",
         "subcategories": []
     },
     {
@@ -28,7 +28,7 @@ category_data = [
         "active": True,
         "for_homepage": True,
         "description": "De perfecte combinatie van stijl, comfort en functionaliteit. Klik vinylvloeren zijn snel en eenvoudig te installeren door het klik-systeem, wat ze ideaal maakt voor doe-het-zelvers. Deze vloeren zijn bestand tegen vocht, gemakkelijk schoon te maken, en bieden een breed scala aan designopties die passen bij iedere ruimte in je huis.",
-        "thumbnail": "category_thumbnails/klik-vinyl-category.jpg",
+        "thumbnail": "category_thumbnails/vinyl-category.jpeg",
         "subcategories": []
     },
     {
@@ -36,7 +36,7 @@ category_data = [
         "active": True,
         "for_homepage": True,
         "description": "Verhoog de afwerking van je vloer en geef je ruimte een nette uitstraling met onze stijlvolle plinten. Onze plinten bieden niet alleen esthetische voordelen, maar helpen ook om je vloeren te beschermen tegen vuil, vocht en beschadigingen. Van klassieke tot moderne ontwerpen, we hebben de juiste plint voor iedere stijl.",
-        "thumbnail": "category_thumbnails/plinten-category.webp",
+        "thumbnail": "category_thumbnails/plinten-category.jpeg",
         "subcategories": []
     },
     {
@@ -44,7 +44,7 @@ category_data = [
         "active": True,
         "for_homepage": True,
         "description": "Het juiste ondervloermateriaal is essentieel voor het behoud van je vloeren en het verbeteren van het comfort in je huis. Onze ondervloeren bieden extra demping, isolatie en bescherming tegen onvolkomenheden in de ondergrond. Ze helpen geluidsoverlast te verminderen en zorgen ervoor dat je vloeren langer meegaan, zonder in te boeten op kwaliteit.",
-        "thumbnail": "category_thumbnails/klik-vinyl-category.jpg",
+        "thumbnail": None,
         "subcategories": []
     },
     {
@@ -52,7 +52,7 @@ category_data = [
         "active": True,
         "for_homepage": False,
         "description": "Ontdek onze best beoordeelde vloeren, gekozen door tevreden klanten. Deze vloeren combineren stijl en duurzaamheid, en worden vaak geprezen om hun lange levensduur en gemakkelijke onderhoud. Als je op zoek bent naar een vloer die niet alleen mooi is, maar ook bestand tegen het dagelijks gebruik, dan is onze collectie hardlopers perfect voor jou.",
-        "thumbnail": "category_thumbnails/hardlopers_category.jpg",
+        "thumbnail": None,
         "subcategories": []
     },
     {
